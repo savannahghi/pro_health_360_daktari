d374e323437ec682738f64bc41c52586f0a45174
Byron Kimani <byronkimani@gmail.com>
2021-09-03 12:08:21 +0300

    chore: restore tests for kyc reg page
    - restore tests for kyc reg page

--- test/widget_tests/presentation/onboarding/kyc/individual/individual_practitioner/individual_practitioner_kyc_step_one_test.dart
+++ test/widget_tests/presentation/onboarding/kyc/individual/individual_practitioner/individual_practitioner_kyc_step_one_test.dart
@@ -4,6 +4,7 @@ import 'package:healthcloud/application/redux/actions/kyc_state_actions/individu
 import 'package:healthcloud/application/redux/states/app_state.dart';
 import 'package:healthcloud/domain/core/value_objects/app_strings.dart';
 import 'package:healthcloud/domain/core/value_objects/app_widget_keys.dart';
+import 'package:healthcloud/presentation/onboarding/practitioner_kyc/common/widgets/kyc_license_reg_page.dart';
 import 'package:healthcloud/presentation/onboarding/practitioner_kyc/individual/individual_practitioner_step_one.dart';
 import 'package:healthcloud/presentation/onboarding/practitioner_kyc/individual/individual_practitioner_step_two.dart';
 import 'package:async_redux/async_redux.dart';
@@ -43,7 +44,7 @@ void main() {
 
         expect(find.text('What is your profession?'), findsOneWidget);
 
-        expect(find.byKey(doctorRadioKey), findsOneWidget);
+        expect(find.byKey(clinicalOfficerRadioKey), findsOneWidget);
 
         await tester.enterText(find.byKey(kycRegNumberKey), '');
         await tester.pump();
@@ -65,7 +66,7 @@ void main() {
     });
 
     testWidgets(
-        'should fill the KYC details correctly and navigate to step two',
+        'should fill the KYC details correctly for a doctor and navigate to step two',
         (WidgetTester tester) async {
       store.dispatch(
         IndividualPractitionerKYCAction(
@@ -92,6 +93,7 @@ void main() {
       );
 
       expect(find.text('What is your profession?'), findsOneWidget);
+      expect(find.byType(KYCLicenceAndRegistrationNumber), findsOneWidget);
       expect(find.byKey(kycRegNumberKey), findsOneWidget);
       expect(find.byKey(kycLicenceNumberKey), findsOneWidget);
       expect(find.byKey(kycLicenceNumberKey), findsOneWidget);
@@ -110,6 +112,186 @@ void main() {
       await tester.pump();
 
       await tester.tap(find.byKey(doctorRadioKey));
+      await tester.pump();
+
+      expect(
+          find.text('Please tick all services that you offer'), findsOneWidget);
+
+      for (int i = 0; i < practiceServicesList.length; i++) {
+        final Finder checkBox =
+            find.byKey(Key(practiceServicesList[i].value), skipOffstage: false);
+        await tester.ensureVisible(checkBox);
+        await tester.tap(checkBox);
+        await tester.pump();
+      }
+
+      expect((store.state as AppState).practitionerKYCState!.cadre, 'NURSE');
+      expect((store.state as AppState).practitionerKYCState!.registrationNumber,
+          'some-reg-number');
+      expect(
+          (store.state as AppState)
+              .practitionerKYCState!
+              .practiceLicenseUploadID,
+          'some-doc-id');
+      expect((store.state as AppState).practitionerKYCState!.practiceLicenseID,
+          'some-id');
+
+      // Perform the license upload
+      final Finder fileUpload =
+          find.byKey(kycLicenceNumUploadKey, skipOffstage: false);
+      await tester.ensureVisible(fileUpload);
+
+      await tester.tap(fileUpload);
+      await tester.pumpAndSettle();
+
+      // expect to find the next button and navigate to step three
+      final Finder nextButton =
+          find.byKey(kycNextButtonKey, skipOffstage: false);
+      await tester.ensureVisible(nextButton);
+
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(IndividualPractitionerKYCStepTwo), findsOneWidget);
+    });
+
+    testWidgets(
+        'should fill the KYC details correctly for a clinical officer and navigate to step two',
+        (WidgetTester tester) async {
+      store.dispatch(
+        IndividualPractitionerKYCAction(
+          practiceLicenseID: 'some-id',
+          practiceLicenseDocID: 'some-doc-id',
+          idNumber: 'some-id',
+          idDocID: 'some-doc-id',
+          idType: 'some-id-type',
+          kraPin: 'some-pin',
+          kraPinDocID: 'some-pin-doc-id',
+          registrationNumber: 'some-reg-number',
+          cadre: 'NURSE',
+          practiceServices: <String>[
+            'OUTPATIENT_SERVICES',
+            'INPATIENT_SERVICES'
+          ],
+        ),
+      );
+
+      await buildTestWidget(
+        tester: tester,
+        store: store,
+        widget: IndividualPractitionerKYCStepOne(),
+      );
+
+      expect(find.text('What is your profession?'), findsOneWidget);
+      expect(find.byType(KYCLicenceAndRegistrationNumber), findsOneWidget);
+      expect(find.byKey(kycRegNumberKey), findsOneWidget);
+      expect(find.byKey(kycLicenceNumberKey), findsOneWidget);
+      expect(find.byKey(kycLicenceNumberKey), findsOneWidget);
+
+      await tester.tap(find.byKey(kycLicenceNumUploadKey));
+      await tester.pumpAndSettle();
+
+      await tester.enterText(find.byKey(kycRegNumberKey), '123456789');
+      await tester.pump();
+
+      await tester.enterText(find.byKey(kycLicenceNumberKey), '123456789');
+      await tester.pumpAndSettle();
+
+      // uncheck confirmed checkbox
+      await tester.tap(find.byKey(Key(practiceServicesList[0].value)));
+      await tester.pump();
+
+      await tester.tap(find.byKey(clinicalOfficerRadioKey));
+      await tester.pump();
+
+      expect(
+          find.text('Please tick all services that you offer'), findsOneWidget);
+
+      for (int i = 0; i < practiceServicesList.length; i++) {
+        final Finder checkBox =
+            find.byKey(Key(practiceServicesList[i].value), skipOffstage: false);
+        await tester.ensureVisible(checkBox);
+        await tester.tap(checkBox);
+        await tester.pump();
+      }
+
+      expect((store.state as AppState).practitionerKYCState!.cadre, 'NURSE');
+      expect((store.state as AppState).practitionerKYCState!.registrationNumber,
+          'some-reg-number');
+      expect(
+          (store.state as AppState)
+              .practitionerKYCState!
+              .practiceLicenseUploadID,
+          'some-doc-id');
+      expect((store.state as AppState).practitionerKYCState!.practiceLicenseID,
+          'some-id');
+
+      // Perform the license upload
+      final Finder fileUpload =
+          find.byKey(kycLicenceNumUploadKey, skipOffstage: false);
+      await tester.ensureVisible(fileUpload);
+
+      await tester.tap(fileUpload);
+      await tester.pumpAndSettle();
+
+      // expect to find the next button and navigate to step three
+      final Finder nextButton =
+          find.byKey(kycNextButtonKey, skipOffstage: false);
+      await tester.ensureVisible(nextButton);
+
+      await tester.tap(nextButton);
+      await tester.pumpAndSettle();
+
+      expect(find.byType(IndividualPractitionerKYCStepTwo), findsOneWidget);
+    });
+
+    testWidgets(
+        'should fill the KYC details correctly for a nurse and navigate to step two',
+        (WidgetTester tester) async {
+      store.dispatch(
+        IndividualPractitionerKYCAction(
+          practiceLicenseID: 'some-id',
+          practiceLicenseDocID: 'some-doc-id',
+          idNumber: 'some-id',
+          idDocID: 'some-doc-id',
+          idType: 'some-id-type',
+          kraPin: 'some-pin',
+          kraPinDocID: 'some-pin-doc-id',
+          registrationNumber: 'some-reg-number',
+          cadre: 'NURSE',
+          practiceServices: <String>[
+            'OUTPATIENT_SERVICES',
+            'INPATIENT_SERVICES'
+          ],
+        ),
+      );
+
+      await buildTestWidget(
+        tester: tester,
+        store: store,
+        widget: IndividualPractitionerKYCStepOne(),
+      );
+
+      expect(find.text('What is your profession?'), findsOneWidget);
+      expect(find.byType(KYCLicenceAndRegistrationNumber), findsOneWidget);
+      expect(find.byKey(kycRegNumberKey), findsOneWidget);
+      expect(find.byKey(kycLicenceNumberKey), findsOneWidget);
+      expect(find.byKey(kycLicenceNumberKey), findsOneWidget);
+
+      await tester.tap(find.byKey(kycLicenceNumUploadKey));
+      await tester.pumpAndSettle();
+
+      await tester.enterText(find.byKey(kycRegNumberKey), '123456789');
+      await tester.pump();
+
+      await tester.enterText(find.byKey(kycLicenceNumberKey), '123456789');
+      await tester.pumpAndSettle();
+
+      // uncheck confirmed checkbox
+      await tester.tap(find.byKey(Key(practiceServicesList[0].value)));
+      await tester.pump();
+
+      await tester.tap(find.byKey(nurseRadioKey));
       await tester.pump();
 
       expect(
