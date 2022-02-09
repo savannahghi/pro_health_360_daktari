28cb53c04a8b8ff09aab4946a32a80f0a6377ad1
Paul Mburu <paulmburu53@gmail.com>
2022-02-09 16:07:33 +0300

    feat: refresh token

--- lib/presentation/core/afya_moja_app.dart
+++ lib/presentation/core/afya_moja_app.dart
@@ -1,15 +1,15 @@
 // Flutter imports:
 
-import 'package:flutter/material.dart';
-
 // Package imports:
 import 'package:app_wrapper/app_wrapper.dart';
 import 'package:async_redux/async_redux.dart';
-import 'package:flutter_graphql_client/graph_client.dart';
-
+import 'package:flutter/material.dart';
 // Project imports:
 import 'package:healthcloud/application/core/services/app_setup_data.dart';
+import 'package:healthcloud/application/core/services/custom_client.dart';
 import 'package:healthcloud/application/redux/states/app_state.dart';
+import 'package:healthcloud/application/redux/view_model_factories/app_entry_point_view_model_factory.dart';
+import 'package:healthcloud/application/redux/view_models/app_entry_point_view_model.dart';
 import 'package:healthcloud/domain/core/value_objects/app_strings.dart';
 import 'package:healthcloud/domain/core/value_objects/app_widget_keys.dart';
 import 'package:healthcloud/infrastructure/connectivity/connectivity_interface.dart';
@@ -34,16 +34,25 @@ class AfyaMojaApp extends StatelessWidget {
     return StoreProvider<AppState>(
       key: globalStoreProviderKey,
       store: store,
-      child: StoreConnector<AppState, AppState>(
-        converter: (Store<AppState> store) => store.state,
-        builder: (BuildContext context, AppState state) {
+      child: StoreConnector<AppState, AppEntryPointViewModel>(
+        vm: () => AppEntryPointViewModelFactory(),
+        builder: (BuildContext context, AppEntryPointViewModel vm) {
+          final String idToken = vm.idToken ?? '';
+          final String graphqlEndpoint =
+              appSetupData.customContext!.graphqlEndpoint;
+          final String refreshTokenEndpoint =
+              appSetupData.customContext?.refreshTokenEndpoint ?? '';
+          final String userID = vm.userId ?? '';
           return AppWrapper(
             appContexts: appSetupData.appContexts,
             appName: appName,
             baseContext: appSetupData.customContext,
-            graphQLClient: GraphQlClient(
-              store.state.credentials?.idToken ?? '',
-              appSetupData.customContext?.graphqlEndpoint ?? '',
+            graphQLClient: CustomClient(
+              idToken,
+              graphqlEndpoint,
+              context: context,
+              refreshTokenEndpoint: refreshTokenEndpoint,
+              userID: userID,
             ),
             child: AuthManager(
               appName: appName,
--- test/widget_tests/presentation/auth_manager_test.dart
+++ test/widget_tests/presentation/auth_manager_test.dart
@@ -4,6 +4,7 @@ import 'package:connectivity_plus_platform_interface/connectivity_plus_platform_
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:healthcloud/application/core/services/custom_client.dart';
 import 'package:healthcloud/application/core/services/helpers.dart';
 import 'package:healthcloud/application/redux/states/app_state.dart';
 import 'package:healthcloud/domain/core/value_objects/app_contexts.dart';
@@ -12,6 +13,7 @@ import 'package:healthcloud/presentation/core/auth_manager.dart';
 import 'package:healthcloud/presentation/router/route_generator.dart';
 
 import '../../mocks/mocks.dart';
+import '../application/redux/actions/user_state_actions/check_token_action_test.dart';
 
 void main() {
   group('Authmanager', () {
@@ -37,22 +39,35 @@ void main() {
       final Store<AppState> store =
           Store<AppState>(initialState: AppState.initial());
 
+      late CustomClient customClient;
+
       await tester.pumpWidget(
         StoreProvider<AppState>(
           store: store,
-          child: AppWrapper(
-            appName: 'test',
-            appContexts: testAppContexts,
-            baseContext: devAppSetupData.customContext,
-            graphQLClient: mockSILGraphQlClient,
-            child: MaterialApp(
-              onGenerateRoute: RouteGenerator.generateRoute,
-              home: AuthManager(
+          child: Builder(
+            builder: (BuildContext context) {
+              customClient = MockCustomClient(
+                '',
+                '',
+                refreshTokenEndpoint: '',
+                userID: '',
+                context: context,
+              );
+              return AppWrapper(
                 appName: 'test',
                 appContexts: testAppContexts,
-                connectivityStatus: connectivityStatus,
-              ),
-            ),
+                baseContext: devAppSetupData.customContext,
+                graphQLClient: customClient,
+                child: MaterialApp(
+                  onGenerateRoute: RouteGenerator.generateRoute,
+                  home: AuthManager(
+                    appName: 'test',
+                    appContexts: testAppContexts,
+                    connectivityStatus: connectivityStatus,
+                  ),
+                ),
+              );
+            },
           ),
         ),
       );
