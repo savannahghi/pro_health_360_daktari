a272362828ab07725bef7485ca99c9e1ef22e245
Paul Mburu <paulmburu53@gmail.com>
2022-02-15 11:41:44 +0300

    feat: shared profile components

--- lib/presentation/profile/pages/user_profile_page.dart
+++ lib/presentation/profile/pages/user_profile_page.dart
@@ -1,22 +1,19 @@
 // Flutter imports:
 import 'package:afya_moja_core/afya_moja_core.dart';
-// Package imports:
-import 'package:afya_moja_core/afya_moja_core.dart' as afyamoja;
 import 'package:async_redux/async_redux.dart';
 import 'package:domain_objects/value_objects.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-// Project imports:
-import 'package:healthcloud/application/core/services/utils.dart';
 import 'package:healthcloud/application/core/theme/app_themes.dart';
 import 'package:healthcloud/application/redux/states/app_state.dart';
 import 'package:healthcloud/application/redux/view_models/app_state_view_model.dart';
 import 'package:healthcloud/domain/core/entities/core/staff_state.dart';
+import 'package:healthcloud/domain/core/value_objects/app_asset_strings.dart';
 import 'package:healthcloud/domain/core/value_objects/app_strings.dart';
 import 'package:healthcloud/presentation/core/app_bar/custom_app_bar.dart';
 import 'package:healthcloud/presentation/core/widgets/logout_button.dart';
 import 'package:healthcloud/presentation/profile/widgets/user_details_card_widget.dart'
     as local;
+import 'package:healthcloud/presentation/router/routes.dart';
 import 'package:shared_themes/spaces.dart';
 
 // Project imports:
@@ -53,42 +50,30 @@ class UserProfilePage extends StatelessWidget {
                     profession: profession,
                   ),
                   mediumVerticalSizedBox,
-
-                  /// [userProfileItems] is a list of the UserProfilePage ListTile items
-                  ...List<Widget>.generate(userProfileItems.length,
-                      (int index) {
-                    final String iconPath =
-                        userProfileItems.elementAt(index).iconAssetPath;
-                    final String title =
-                        userProfileItems.elementAt(index).title;
-
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 4),
-                      child: afyamoja.InformationListCard(
-                        title: Text(
-                          title,
-                          maxLines: 2,
-                          style: afyamoja.veryBoldSize14Text(
-                            Theme.of(context).primaryColor,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        iconBackgroundColor: AppColors.profileListCardColor,
-                        leadingIcon: Container(
-                          width: 25,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: SvgPicture.asset(
-                            iconPath,
-                            width: 20,
-                            height: 20,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                        ),
-                      ),
-                    );
-                  }),
+                  const UserProfileItem(
+                    iconPath: helpCircleIcon,
+                    title: 'Help/Contact Admin',
+                    routeName: AppRoutes.surveysPage,
+                    itemBackgroundColor: AppColors.profileListCardColor,
+                  ),
+                  const UserProfileItem(
+                    iconPath: feedbackIconSvgPath,
+                    title: 'Resolved Requests',
+                    routeName: AppRoutes.surveysPage,
+                    itemBackgroundColor: AppColors.profileListCardColor,
+                  ),
+                  const UserProfileItem(
+                    iconPath: faqsIconSvgPath,
+                    title: 'FAQs',
+                    routeName: AppRoutes.surveysPage,
+                    itemBackgroundColor: AppColors.profileListCardColor,
+                  ),
+                  const UserProfileItem(
+                    iconPath: settingsIconSvgPath,
+                    title: 'Settings',
+                    routeName: AppRoutes.surveysPage,
+                    itemBackgroundColor: AppColors.profileListCardColor,
+                  ),
                   mediumVerticalSizedBox,
                   const SizedBox(
                     width: double.infinity,
