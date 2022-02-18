45010838a877eadbd4a6206722432dbc49cfc707
Eugene <eugenemrlens@gmail.com>
2022-02-18 15:38:38 +0300

    chore: error handling on red flags page

--- test/widget_tests/presentation/service_requests/service_requests_page_test.dart
+++ test/widget_tests/presentation/service_requests/service_requests_page_test.dart
@@ -1,5 +1,9 @@
 // Package imports:
+import 'dart:convert';
+
+import 'package:async_redux/async_redux.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:healthcloud/application/redux/states/app_state.dart';
 import 'package:healthcloud/domain/core/entities/service_requests/pending_service_requests.dart';
 
 // Project imports:
@@ -7,12 +11,19 @@ import 'package:healthcloud/domain/core/value_objects/app_strings.dart';
 import 'package:healthcloud/domain/core/value_objects/app_widget_keys.dart';
 import 'package:healthcloud/presentation/core/app_bar/custom_app_bar.dart';
 import 'package:healthcloud/presentation/engagement/home/widgets/action_card.dart';
+import 'package:healthcloud/presentation/service_requests/pages/red_flags_page.dart';
 import 'package:healthcloud/presentation/service_requests/pages/service_requests_page.dart';
+import 'package:http/http.dart';
 import '../../../mocks/mocks.dart';
 import '../../../mocks/test_helpers.dart';
 
 void main() {
   group('ServiceRequestsPage', () {
+    late Store<AppState> store;
+
+    setUp(() {
+      store = Store<AppState>(initialState: AppState.initial());
+    });
     testWidgets('should render correctly with default values',
         (WidgetTester tester) async {
       await buildTestWidget(
@@ -31,7 +42,6 @@ void main() {
       await tester.tap(find.byType(ActionCard).first);
       await tester.pumpAndSettle();
       expect(find.text(serviceRequestString), findsNothing);
-
       await tester.tap(find.byKey(appBarBackButtonKey));
       await tester.pumpAndSettle();
       expect(find.text(serviceRequestString), findsOneWidget);
@@ -58,6 +68,41 @@ void main() {
       await tester.tap(find.byType(ActionCard));
       await tester.pumpAndSettle();
       expect(find.text(serviceRequestString), findsNothing);
+    });
+    testWidgets('red flags is tappable', (WidgetTester tester) async {
+      final MockShortGraphQlClient mockShortGraphQlClient =
+          MockShortGraphQlClient.withResponse(
+        'idToken',
+        'endpoint',
+        Response(
+          json.encode(<String, dynamic>{
+            'data': <String, dynamic>{'getServiceRequests': <dynamic>[]}
+          }),
+          201,
+        ),
+      );
+      await buildTestWidget(
+        tester: tester,
+        graphQlClient: mockShortGraphQlClient,
+        store: store,
+        widget: ServiceRequestsPage(
+          pendingServiceRequest:
+              PendingServiceRequest.fromJson(<String, dynamic>{
+            'total': 2,
+            'requestsTypeCount': <dynamic>[
+              <String, dynamic>{'requestType': 'RED_FLAG', 'total': 2},
+            ],
+          }),
+        ),
+      );
+      final Finder genericNoDataButton = find.byKey(genericNoDataButtonKey);
+
+      await tester.tap(find.byType(ActionCard));
+      await tester.pumpAndSettle();
+
+      await tester.tap(genericNoDataButton);
+      await tester.pumpAndSettle();
+      expect(find.byType(RedFlagsPage), findsNothing);
     });
   });
 }
