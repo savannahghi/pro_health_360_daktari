fa3348cd4cd338ec1a08802112b692b74b849f98
Eugene <eugenemrlens@gmail.com>
2022-03-09 08:09:38 +0300

    feat: add group icon to channel page

--- lib/presentation/communities/channel_page.dart
+++ lib/presentation/communities/channel_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:myharehubpro/domain/core/value_objects/app_asset_strings.dart';
 import 'package:myharehubpro/presentation/communities/thread_page.dart';
 import 'package:myharehubpro/presentation/router/routes.dart';
+import 'package:shared_themes/spaces.dart';
 import 'package:stream_chat_flutter/stream_chat_flutter.dart';
 
 class ChannelPage extends StatelessWidget {
@@ -10,9 +13,14 @@ class ChannelPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final Channel channel = StreamChannel.of(context).channel;
     String channelName = 'No title';
+    String channelImage = '';
 
     if (channel.extraData.containsKey('Name')) {
       channelName = channel.extraData['Name']! as String;
+    }
+
+    if (channel.extraData.containsKey('image')) {
+      channelImage = channel.extraData['image']! as String;
     }
 
     return Scaffold(
@@ -23,31 +31,28 @@ class ChannelPage extends StatelessWidget {
         ),
         showConnectionStateTile: true,
         actions: <Widget>[
-          PopupMenuButton<ChannelOptions>(
-            onSelected: (ChannelOptions value) {
-              switch (value) {
-                case ChannelOptions.invite_client:
-                  Navigator.of(context).pushNamed(
-                    AppRoutes.inviteMembersPage,
-                    arguments: StreamChannel.of(context).channel.id,
-                  );
-                  break;
-                default:
-              }
-            },
-            itemBuilder: (BuildContext context) {
-              return <PopupMenuItem<ChannelOptions>>[
-                const PopupMenuItem<ChannelOptions>(
-                  value: ChannelOptions.invite_client,
-                  child: Text('Invite members'),
-                )
-              ];
-            },
-            child: const Icon(
-              Icons.more_vert,
-              color: Colors.black,
+          GestureDetector(
+            onTap: () => Navigator.of(context).pushNamed(
+              AppRoutes.inviteMembersPage,
+              arguments: StreamChannel.of(context).channel.id,
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ClipOval(
+                child: channelImage.isNotEmpty
+                    ? Image.network(
+                        channelImage,
+                        fit: BoxFit.cover,
+                      )
+                    : SvgPicture.asset(
+                        groupInfoImage,
+                        height: 30,
+                        width: 30,
+                      ),
+              ),
             ),
           ),
+          size15HorizontalSizedBox,
         ],
       ),
       body: Column(
