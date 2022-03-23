d9a1b1b7748e33ac0a702e97359cdc5e94468788
Byron Kimani <byronkimani@gmail.com>
2022-03-23 16:13:17 +0300

    chore: fix search detail view
    - fix search detail view
    
    Signed-off-by: Byron Kimani <byronkimani@gmail.com>

--- lib/presentation/search/widgets/client_search_widget.dart
+++ lib/presentation/search/widgets/client_search_widget.dart
@@ -2,14 +2,12 @@ import 'package:afya_moja_core/afya_moja_core.dart';
 import 'package:app_wrapper/app_wrapper.dart';
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:mycarehubpro/application/core/theme/app_themes.dart';
 import 'package:mycarehubpro/application/redux/actions/flags/app_flags.dart';
 import 'package:mycarehubpro/application/redux/actions/search_users/invite_client_action.dart';
 import 'package:mycarehubpro/application/redux/states/app_state.dart';
 import 'package:mycarehubpro/application/redux/view_models/app_state_view_model.dart';
 import 'package:mycarehubpro/domain/core/entities/search_user/search_user_response.dart';
-import 'package:mycarehubpro/domain/core/value_objects/app_asset_strings.dart';
 import 'package:mycarehubpro/domain/core/value_objects/app_strings.dart';
 import 'package:mycarehubpro/presentation/router/routes.dart';
 import 'package:mycarehubpro/presentation/search/widgets/search_details_information_widget.dart';
@@ -22,7 +20,6 @@ class ClientSearchWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final String username = searchUserResponse.user?.userName ?? UNKNOWN;
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
@@ -106,33 +103,6 @@ class ClientSearchWidget extends StatelessWidget {
                   },
                 ),
                 mediumVerticalSizedBox,
-                Text(
-                  '2. $consent ',
-                  style: boldSize15Text(AppColors.greyTextColor),
-                ),
-                verySmallVerticalSizedBox,
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                  child: Text(
-                    '${username.split(' ').first} $hasAgreedToUse',
-                    style: normalSize14Text(AppColors.greyTextColor),
-                  ),
-                ),
-                smallVerticalSizedBox,
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 18.0),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      SvgPicture.asset(doneIconSvgPath),
-                      verySmallHorizontalSizedBox,
-                      Text(
-                        consentRecorded,
-                        style: normalSize12Text(AppColors.malachiteColor),
-                      )
-                    ],
-                  ),
-                )
               ],
             ),
           ),
