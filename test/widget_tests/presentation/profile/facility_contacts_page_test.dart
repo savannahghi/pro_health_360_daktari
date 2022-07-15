7b7d84f72fbecd7b3e2ed6a2f8e9882a4bd352d7
Eugene <eugenemrlens@gmail.com>
2022-07-15 12:20:38 +0300

    chore: disable update contact for other staff

--- test/widget_tests/presentation/profile/facility_contacts_page_test.dart
+++ test/widget_tests/presentation/profile/facility_contacts_page_test.dart
@@ -1,9 +1,11 @@
+import 'package:afya_moja_core/afya_moja_core.dart';
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:prohealth360_daktari/application/redux/states/app_state.dart';
 import 'package:prohealth360_daktari/domain/core/entities/core/facility.dart';
 import 'package:prohealth360_daktari/domain/core/entities/core/staff_state.dart';
+import 'package:prohealth360_daktari/domain/core/entities/core/user.dart';
 import 'package:prohealth360_daktari/domain/core/value_objects/app_strings.dart';
 import 'package:prohealth360_daktari/presentation/profile/pages/add_facility_contact_page.dart';
 import 'package:prohealth360_daktari/presentation/profile/pages/facility_contacts_page.dart';
@@ -23,27 +25,38 @@ void main() {
             facilities: <Facility>[
               Facility(name: 'test', id: 'test', phone: '0712345678')
             ],
+            user: User(
+              roles: <Role>[
+                Role(
+                  active: true,
+                  name: RoleValue.SYSTEM_ADMINISTRATOR,
+                  roleID: 'test',
+                )
+              ],
+            ),
           ),
         ),
       );
     });
 
     testWidgets('renders correctly', (WidgetTester tester) async {
-      await buildTestWidget(
-        tester: tester,
-        store: store,
-        widget: Builder(
-          builder: (BuildContext context) {
-            return FacilityContactsPage();
-          },
-        ),
-      );
-      await tester.pumpAndSettle();
-      final Finder updateContactBtn = find.text(updateContactString);
-      await tester.ensureVisible(updateContactBtn);
-      await tester.tap(updateContactBtn);
-      await tester.pumpAndSettle();
-      expect(find.byType(AddFacilityContactPage), findsOneWidget);
+      await tester.runAsync(() async {
+        await buildTestWidget(
+          tester: tester,
+          store: store,
+          widget: Builder(
+            builder: (BuildContext context) {
+              return FacilityContactsPage();
+            },
+          ),
+        );
+        await tester.pumpAndSettle();
+        final Finder updateContactBtn = find.text(updateContactString);
+        await tester.ensureVisible(updateContactBtn);
+        await tester.tap(updateContactBtn);
+        await tester.pumpAndSettle();
+        expect(find.byType(AddFacilityContactPage), findsOneWidget);
+      });
     });
   });
 }
